36c6aabc4e2edaed8c209f8dd4e6d517adb89e76
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-04 10:30:28 +0200

    Setup removeTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -43,6 +43,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID.
+    const remainingTasks = tasks.filter((task)=> { return task.id !== id })
+    setTasks(remainingTasks)
   }
 
   return (
